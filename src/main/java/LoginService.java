fe862ffe892f9e2802eb8912c589d7cc0e2f41b6
Hakim Akbary <hakim.akbary1984@gmail.com>
2021-02-11 19:38:26 +0100

    more changes made here

--- src/main/java/LoginService.java
+++ src/main/java/LoginService.java
@@ -1,4 +1,7 @@
 public class LoginService {
     //added some code for login serviceb21
     //some comment
+    public static void main(String[] args) {
+        System.out.println("ready to go back to living room");
+    }
 }
